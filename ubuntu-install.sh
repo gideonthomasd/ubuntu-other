772b79b1e7ff2e70275b68aade73f5ceab38dc4e
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-10-01 14:52:55 +0100

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+sudo apt update && sudo apt upgrade
+
 sudo apt install xorg xserver-xorg xutils mesa-utils xinit openbox obconf i3 dmenu lxappearance git sakura xterm feh nitrogen thunar materia-gtk-theme deepin-icon-theme network-manager -yy
 
-sudo apt install tint2 xcompmgr geany unzip build-essential pulseaudio pavucontrol lightdm fonts-font-awesome rofi gsimplecal curl lemonbar sxhkd wget youtube-dl plank lxtask lxpolkit vlc spectrwm -yy
+sudo apt install tint2 xcompmgr geany unzip pulseaudio pavucontrol fonts-font-awesome rofi gsimplecal curl lemonbar sxhkd wget youtube-dl plank lxtask lxpolkit vlc -yy
 
 sudo apt install parcellite lxterminal vim i3blocks breeze-cursor-theme breeze-gtk-theme breeze-icon-theme gparted ark neofetch flatpak i3lock-fancy -yy
 
-sudo apt install lightdm-gtk-greeter lightdm-gtk-greeter-settings polybar -yy
+sudo apt install libx11-dev  build-essential libxinerama-dev sharutils suckless-tools libxft-dev moc python3-pip fonts-powerline -yy
+
+################Install dwm################################
+
+
