e171c9192757cfecd12815fd285346baa0602e6f
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-10-03 17:53:33 +0100

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,14 +4,14 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install xorg xserver-xorg xutils mesa-utils xinit openbox obconf i3 dmenu lxappearance git sakura xterm feh nitrogen thunar materia-gtk-theme deepin-icon-theme network-manager -yy
 
-sudo apt install tint2 xcompmgr geany unzip pulseaudio pavucontrol fonts-font-awesome rofi gsimplecal curl lemonbar sxhkd wget plank lxtask lxpolkit vlc -yy
+sudo apt install tint2 xcompmgr geany unzip pulseaudio pavucontrol fonts-font-awesome rofi gsimplecal curl sxhkd wget plank lxtask lxpolkit vlc -yy
 
 sudo apt install parcellite lxterminal vim i3blocks breeze-cursor-theme breeze-gtk-theme breeze-icon-theme gparted ark neofetch flatpak i3lock-fancy -yy
 
 sudo apt install libx11-dev  build-essential libxinerama-dev sharutils suckless-tools libxft-dev moc python3-pip fonts-powerline fonts-noto lightdm -yy
 
 ################ Prepare succade & lemonbar-xft#####################
-sudo apt install build-essential libx11-dev libxft-dev libx11-xcb-dev libxcb-randr0-dev libxcb-xinerama0-dev -yy
+sudo apt install build-essential libx11-dev libxft-dev libx11-xcb-dev libxcb-randr0-dev libxcb-xinerama0-dev bspwm -yy
 sudo apt install stalonetray volumeicon-alsa -yy
 
 mkdir -p $HOME"/lemonbar-xft"
